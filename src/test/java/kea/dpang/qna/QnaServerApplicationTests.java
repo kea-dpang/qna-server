ffe4544fd4b371da18d850a1207a9fda278716fe
Seok-Hun <hamer1233@naver.com>
2024-01-12 11:49:07 +0900

    Feat: qna-service repo 생성 qna service project build

---
+++ src/test/java/kea/dpang/qna/QnaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kea.dpang.qna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QnaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

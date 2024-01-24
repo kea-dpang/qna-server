8e2ad4bb391f4e7504410a4f6d48cc4b5992e9bf
SeungHyeon <namsh1125@naver.com>
2024-01-24 22:05:46 +0900

    Refactor: application.properties 리팩토링

---
+++ src/main/java/kea/dpang/qna/config/SwaggerConfig.java
@@ -0,0 +1,47 @@
+package kea.dpang.qna.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.customizers.OpenApiCustomizer;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+
+/**
+ * Swagger(Spring Docs) 설정
+ */
+@OpenAPIDefinition(
+        info = @Info(title = "DPANG QNA API 명세서",
+                description = "DPANG QNA API 명세서",
+                version = "0.2.0")
+)
+@RequiredArgsConstructor
+@Configuration
+public class SwaggerConfig {
+
+    public OpenApiCustomizer securityBudiler() {
+        return openApi -> openApi.addSecurityItem(new SecurityRequirement().addList("Access Token"))
+                .getComponents()
+                .addSecuritySchemes("Access Token", new SecurityScheme()
+                        .name(HttpHeaders.AUTHORIZATION)
+                        .type(SecurityScheme.Type.HTTP)
+                        .in(SecurityScheme.In.HEADER)
+                        .bearerFormat("JWT")
+                        .scheme("bearer"));
+    }
+
+    @Bean
+    public GroupedOpenApi openApi() {
+        String[] paths = {"/**"};
+
+        return GroupedOpenApi.builder()
+                .group("DPANG QNA 서비스 API")
+                .pathsToMatch(paths)
+                .addOpenApiCustomizer(securityBudiler())
+                .build();
+    }
+}

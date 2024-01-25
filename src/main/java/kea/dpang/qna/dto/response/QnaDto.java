0e450e3240650258aa486ee5d8e290da8853770d
SeungHyeon <namsh1125@naver.com>
2024-01-25 14:11:01 +0900

    Fix: DTO 직렬화 문제 해결

--- src/main/java/kea/dpang/qna/dto/response/QnaDto.java
+++ src/main/java/kea/dpang/qna/dto/response/QnaDto.java
@@ -4,14 +4,16 @@ import kea.dpang.qna.entity.Category;
 import kea.dpang.qna.entity.Status;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 
 import java.time.LocalDateTime;
 
 /**
  * QnA의 간략한 정보를 담는 DTO 클래스
  */
-@AllArgsConstructor
+@Data
 @Builder
+@AllArgsConstructor
 public class QnaDto {
     private Long qnaId;  // QnA의 ID
     private Category category;  // QnA의 카테고리

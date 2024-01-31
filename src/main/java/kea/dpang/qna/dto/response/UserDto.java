59ed224f208e3b574aef148372b690f8a911e905
Seok-Hun <hamer1233@naver.com>
2024-01-31 15:58:07 +0900

    Fix: 페인 연결 데이터 수정 UserDto에 어노테이션 추가

--- src/main/java/kea/dpang/qna/dto/response/UserDto.java
+++ src/main/java/kea/dpang/qna/dto/response/UserDto.java
@@ -1,5 +1,10 @@
 package kea.dpang.qna.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class UserDto {
     private String name;
     private String email;

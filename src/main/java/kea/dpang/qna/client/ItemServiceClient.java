19a551866a5c71684903c5e773abb7045d77852c
Seok-Hun <hamer1233@naver.com>
2024-02-06 21:24:43 +0900

    Fix: 상품 서버에 요청하는 페인 수정 상품 서버에서 findName 링크가 사라져 상품 상세 조회 API로 변경

--- src/main/java/kea/dpang/qna/client/ItemServiceClient.java
+++ src/main/java/kea/dpang/qna/client/ItemServiceClient.java
@@ -1,6 +1,7 @@
 package kea.dpang.qna.client;
 
 import kea.dpang.qna.base.SuccessResponse;
+import kea.dpang.qna.client.dto.RequestItemServiceDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "item-server")
 public interface ItemServiceClient {
-    @GetMapping("api/items/findName")
-    ResponseEntity<SuccessResponse<String>> getItemName(@RequestParam Long itemId);
+    @GetMapping("/api/items/{itemId}")
+    ResponseEntity<SuccessResponse<RequestItemServiceDto>> getItemName(@PathVariable Long itemId);
 }
